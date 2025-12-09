5004af99d1b544c32387c1e9dd25472b2baf9b1b
LeandroCaetano10 <lecaetanogimenez@hotmail.com>
2025-12-09 15:41:27 -0300

    Atualização do Exercicio 002

--- PythonProject/Exercicio 002.py
+++ PythonProject/Exercicio 002.py
@@ -1,3 +1,3 @@
+# Programa pergunta nome do usuário dando espaço para ele preencher seu nome, e após um print exibindo o prazer em conhecer.
 nome = input('Digite seu nome: ')
-#print('É um prazer te conhecer', nome, '!')
 print('É um prazer te conhecer {} !'.format(nome))
